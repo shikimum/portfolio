9175fa76e153945547a544de7b59397f5e086106
shikimum <ricchi_ww58eeurdys-9@yahoo.co.jp>
2023-12-13 15:50:31 +0900

    ランキング機能の作成1

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
 
   resources :users, only: %i[new create]
   resource :profile, only: %i[show edit update]
+  resources :rankings, only: %i[index]
+
 
   get 'login', to: 'user_sessions#new'
   post 'login', to: 'user_sessions#create'
