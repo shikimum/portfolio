5afe96f7d1514b671d930caeec478ed23d629f60
shikimum <ricchi_ww58eeurdys-9@yahoo.co.jp>
2024-01-11 10:15:20 +0900

    ランキング機能

--- app/controllers/rankings_controller.rb
+++ app/controllers/rankings_controller.rb
@@ -1,3 +1,5 @@
 class RankingsController < ApplicationController
-  def index; end
+  def index
+    @ranking = Manga.left_joins(:likes).group(:id).order('COUNT(likes.id) DESC').limit(10)
+  end
 end
--- app/models/manga.rb
+++
@@ -1,6 +0,0 @@
-class Manga < ApplicationRecord
-  belongs_to :fatigue_type
-  has_many :reviews, dependent: :destroy
-  has_many :likes, dependent: :destroy
-  has_many :users, through: :likes
-end
