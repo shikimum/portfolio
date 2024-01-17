43c23d39890040cb3415080b18afb286261a82aa
shikimum <ricchi_ww58eeurdys-9@yahoo.co.jp>
2024-01-17 19:45:46 +0900

    パスワードリセット設定

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,6 +64,18 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options   = { host: 'https://comic-health-df775b637c30.herokuapp.com' }
+  ActionMailer::Base.smtp_settings           = {
+      address:              ENV["MAILERTOGO_SMTP_HOST"],
+      port:                  ENV["MAILERTOGO_SMTP_PORT"],
+      authentication:       :plain,
+      user_name:            ENV['MAILERTOGO_SMTP_USER'],
+      password:             ENV['MAILERTOGO_SMTP_PASSWORD'],
+      domain:               'heroku.com',
+      enable_starttls_auto: true
+  }
+
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
