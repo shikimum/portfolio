62d66700120f3911d2758c8db8e3c480f95bb6d2
shikimum <ricchi_ww58eeurdys-9@yahoo.co.jp>
2024-02-05 19:40:43 +0900

    修正

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -16,6 +16,9 @@ SimpleCov.start
 #
 # See https://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 RSpec.configure do |config|
+  config.before(:suite) do
+    load Rails.root.join('db', 'seeds.rb')
+  end
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
   # assertions if you prefer.
