9c927daccb34001d30409dccb53063e5bbcd7789
shikimum <ricchi_ww58eeurdys-9@yahoo.co.jp>
2024-02-14 13:52:16 +0900

    ランキングページにサムネイルを追加

--- app/controllers/rankings_controller.rb
+++ app/controllers/rankings_controller.rb
@@ -1,5 +1,17 @@
 class RankingsController < ApplicationController
   def index
     @ranking = Manga.left_joins(:likes).group(:id).having("COUNT(likes.id > 0)").order('COUNT(likes.id) DESC').limit(3)
+    @ranking.each do |manga|
+      uri = URI.parse('https://www.googleapis.com')
+      http_client = Net::HTTP.new(uri.host, uri.port)
+      get_request = Net::HTTP::Get.new("/books/v1/volumes/#{manga.google_book_api_id}", 'Content-Type' => 'application/json')
+      http_client.use_ssl = true
+      response = http_client.request(get_request)
+      @data = JSON.parse(response.body)
+      manga.thumbnail = @data.dig("volumeInfo", "imageLinks", "thumbnail")
+    rescue StandardError => e
+      logger.error("Google APIでエラーが起こりました")
+      logger.error(e.message)
+    end
   end
 end
